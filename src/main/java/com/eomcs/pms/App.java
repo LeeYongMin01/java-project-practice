fb30af318f81db22abcc81cfabd97da46eb553ca
LeeYongMin01 <vbn0213@gmail.com>
2020-11-19 20:39:47 +0900

    11/19

--- src/main/java/com/eomcs/pms/App.java
+++ src/main/java/com/eomcs/pms/App.java
@@ -3,9 +3,11 @@ package com.eomcs.pms;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import com.eomcs.pms.domain.Board;
 import com.eomcs.pms.domain.Member;
@@ -16,6 +18,7 @@ import com.eomcs.pms.handler.BoardDeleteCommand;
 import com.eomcs.pms.handler.BoardDetailCommand;
 import com.eomcs.pms.handler.BoardListCommand;
 import com.eomcs.pms.handler.BoardUpdateCommand;
+import com.eomcs.pms.handler.Command;
 import com.eomcs.pms.handler.HelloCommand;
 import com.eomcs.pms.handler.MemberAddCommand;
 import com.eomcs.pms.handler.MemberDeleteCommand;
@@ -38,79 +41,64 @@ public class App {
 
   public static void main(String[] args) {
 
+    Map<String,Command> commandMap = new HashMap<>();
+
     List<Board> boardList = new ArrayList<>();
-    BoardAddCommand boardAddCommand = new BoardAddCommand(boardList);
-    BoardListCommand boardListCommand = new BoardListCommand(boardList);
-    BoardDetailCommand boardDetailCommand = new BoardDetailCommand(boardList);
-    BoardUpdateCommand boardUpdateCommand = new BoardUpdateCommand(boardList);
-    BoardDeleteCommand boardDeleteCommand = new BoardDeleteCommand(boardList);
+    commandMap.put("/board/add", new BoardAddCommand(boardList));
+    commandMap.put("/board/list", new BoardListCommand(boardList));
+    commandMap.put("/board/detail", new BoardDetailCommand(boardList));
+    commandMap.put("/board/update", new BoardUpdateCommand(boardList));
+    commandMap.put("/board/delete", new BoardDeleteCommand(boardList));
 
-    List<Member> memberList = new ArrayList<>();
-    MemberAddCommand memberAddCommand = new MemberAddCommand(memberList);
+    List<Member> memberList = new LinkedList<>();
     MemberListCommand memberListCommand = new MemberListCommand(memberList);
-    MemberDetailCommand memberDetailCommand = new MemberDetailCommand(memberList);
-    MemberUpdateCommand memberUpdateCommand = new MemberUpdateCommand(memberList);
-    MemberDeleteCommand memberDeleteCommand = new MemberDeleteCommand(memberList);
+    commandMap.put("/member/add", new MemberAddCommand(memberList));
+    commandMap.put("/member/list", memberListCommand);
+    commandMap.put("/member/detail", new MemberDetailCommand(memberList));
+    commandMap.put("/member/update", new MemberUpdateCommand(memberList));
+    commandMap.put("/member/delete", new MemberDeleteCommand(memberList));
 
-    List<Project> projectList = new ArrayList<>();
-    ProjectAddCommand projectAddCommand = new ProjectAddCommand(projectList, memberListCommand);
-    ProjectListCommand projectListCommand = new ProjectListCommand(projectList);
-    ProjectDetailCommand projectDetailCommand = new ProjectDetailCommand(projectList);
-    ProjectUpdateCommand projectUpdateCommand = new ProjectUpdateCommand(projectList, memberListCommand);
-    ProjectDeleteCommand projectDeleteCommand = new ProjectDeleteCommand(projectList);
+    List<Project> projectList = new LinkedList<>();
+    commandMap.put("/project/add", new ProjectAddCommand(projectList, memberListCommand));
+    commandMap.put("/project/list", new ProjectListCommand(projectList));
+    commandMap.put("/project/detail", new ProjectDetailCommand(projectList));
+    commandMap.put("/project/update", new ProjectUpdateCommand(projectList, memberListCommand));
+    commandMap.put("/project/delete", new ProjectDeleteCommand(projectList));
 
     List<Task> taskList = new ArrayList<>();
-    TaskAddCommand taskAddCommand = new TaskAddCommand(taskList, memberListCommand);
-    TaskListCommand taskListCommand = new TaskListCommand(taskList);
-    TaskDetailCommand taskDetailCommand = new TaskDetailCommand(taskList);
-    TaskUpdateCommand taskUpdateCommand = new TaskUpdateCommand(taskList, memberListCommand);
-    TaskDeleteCommand taskDeleteCommand = new TaskDeleteCommand(taskList);
-    
-    HelloCommand helloCommand = new HelloCommand();
-    
+    commandMap.put("/task/add", new TaskAddCommand(taskList, memberListCommand));
+    commandMap.put("/task/list", new TaskListCommand(taskList));
+    commandMap.put("/task/detail", new TaskDetailCommand(taskList));
+    commandMap.put("/task/update", new TaskUpdateCommand(taskList, memberListCommand));
+    commandMap.put("/task/delete", new TaskDeleteCommand(taskList));
+
+    commandMap.put("/hello", new HelloCommand());
+
     Deque<String> commandStack = new ArrayDeque<>();
 
     Queue<String> commandQueue = new LinkedList<>();
 
     loop:
       while (true) {
-        String command = Prompt.inputString("명령> ");
+        String inputStr = Prompt.inputString("명령> ");
 
-        commandStack.push(command);
-        commandQueue.offer(command);
+        commandStack.push(inputStr);
+        commandQueue.offer(inputStr);
 
-        switch (command) {
-          case "/member/add": memberAddCommand.execute(); break;
-          case "/member/list": memberListCommand.execute(); break;
-          case "/member/detail": memberDetailCommand.execute(); break;
-          case "/member/update": memberUpdateCommand.execute(); break;
-          case "/member/delete": memberDeleteCommand.execute(); break;
-          case "/project/add": projectAddCommand.execute(); break;
-          case "/project/list": projectListCommand.execute(); break;
-          case "/project/detail": projectDetailCommand.execute(); break;
-          case "/project/update": projectUpdateCommand.execute(); break;
-          case "/project/delete": projectDeleteCommand.execute(); break;
-          case "/task/add": taskAddCommand.execute(); break;
-          case "/task/list": taskListCommand.execute(); break;
-          case "/task/detail": taskDetailCommand.execute(); break;
-          case "/task/update": taskUpdateCommand.execute(); break;
-          case "/task/delete": taskDeleteCommand.execute(); break;
-          case "/board/add": boardAddCommand.execute(); break;
-          case "/board/list": boardListCommand.execute(); break;
-          case "/board/detail": boardDetailCommand.execute(); break;
-          case "/board/update": boardUpdateCommand.execute(); break;
-          case "/board/delete": boardDeleteCommand.execute(); break;
-          case "/hello": helloCommand.execute(); break;
-          
+        switch (inputStr) {
           case "history": printCommandHistory(commandStack.iterator()); break;
           case "history2": printCommandHistory(commandQueue.iterator()); break;
-
           case "quit":
           case "exit":
             System.out.println("안녕!");
             break loop;
           default:
+            Command command = commandMap.get(inputStr);
+            if(command != null) {
+              command.execute();
+            } else {
             System.out.println("실행할 수 없는 명령입니다.");
+            }
         }
         System.out.println();
       }
